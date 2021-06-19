9523747da768054b46b517769aa8964fd3af1805
GuidoAmbiorix <84160468+GuidoAmbiorix@users.noreply.github.com>
2021-06-18 21:17:31 -0400

    Practica

--- Clase 11/Ejercicio - Formulario y DOM/Main.js
+++ Clase 11/Ejercicio - Formulario y DOM/Main.js
@@ -9,26 +9,17 @@
 
    buton.addEventListener('click',(e) =>{
 
-    let random = Math.random().toString(16).substring(2,8);
-   
-
     const ancho = document.getElementById("ancho");
     const altura = document.getElementById("altura");
-    const randomColor = document.getElementById("Random");
 
    const contenedor = document.getElementById("contenedor");
 
    contenedor.style.width = ancho.value+"px";
    contenedor.style.height = altura.value+"px";
 
-
     contenedor.innerHTML = "El nuevo ancho es: "+ancho.value+"px Y el nuevo alto es: "+altura.value+"px";
-    contenedor.style.background = "#" + random;
-    contenedor.style.color = 'white';
-    randomColor.value = "#" + random;
-    randomColor.style.color = "white";
-    randomColor.style.background = '#' + random; 
-
+   
+    randomColor(contenedor);
 
     contenedor.style.transitionProperty = 'width,height,background';
     contenedor.style.transitionDuration = '2s';
@@ -36,3 +27,18 @@
     randomColor.style.transitionDuration = '2s';
 
 })
+
+
+function randomColor(contenedor){
+
+  let random = Math.random().toString(16).substring(2,8);
+
+  const randomColor = document.getElementById("Random");
+
+  contenedor.style.background = "#" + random;
+  contenedor.style.color = 'white';
+  randomColor.value = "#" + random;
+  randomColor.style.color = "white";
+  randomColor.style.background = '#' + random; 
+
+}
